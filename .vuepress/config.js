2faecda9dd174bc2b8d7065abeaaf18bdfb2e83b
hejianxian <531601727@qq.com>
2019-12-08 18:56:16 +0800

    chore: add cli feature

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    title: 'Fedd',
-    description: '恒拓高科前端开发文档',
+    title: 'FEDD 前端开发手册'',
+    description: '恒拓高科前端开发手册',
     base: '/fedd/',
+    editLinks: true,
+    editLinkText: '改善此页面',
 
     // Theme
     themeConfig: {
@@ -10,7 +12,7 @@ module.exports = {
         repo: 'https://github.com/WorkPlusFE/fedd',
 
         nav: [
-            { text: '规范及约定', link: '/' },
+            { text: '规范及约定', link: '/standard' },
             { text: '开发流程', link: '/dev-flow' },
             { text: '公共库/工具', link: '/packages' },
         ],
